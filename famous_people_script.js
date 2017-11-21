f8b61f1e8350f4f97cafb2241608d24b98387112
bhavBains <deepbains26@gmail.com>
2017-11-21 21:58:35 +0000

    added new script file

---
+++ famous_people_script.js
@@ -0,0 +1,44 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+const argv = process.argv.slice(2);
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+function getFirstName( name, cb) {
+  client.query("SELECT * FROM famous_people WHERE first_name = $1", [name], cb);
+}
+
+function getLastName( name, cb)  {
+  client.query("SELECT * FROM famous_people WHERE last_name = $1", [name], cb);
+}
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  getFirstName(argv[0], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(result.rows);
+  });
+
+  getLastName(argv[0], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(result.rows);
+  client.end();
+  });
+
+  
+});
+
+
