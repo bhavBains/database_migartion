8a4e8040f4f20c4093b5f3b2160ae39fd4e055c7
bhavBains <deepbains26@gmail.com>
2017-11-22 00:52:46 +0000

    added new script using knex

---
+++ knex_script.js
@@ -0,0 +1,20 @@
+const settings = require("./settings"); // settings.json
+const argv = process.argv.slice(2);
+
+const pg = require('knex')({
+  client: 'pg',
+  connection: {
+	  user     : settings.user,
+	  password : settings.password,
+	  database : settings.database,
+	  host     : settings.hostname,
+	  port     : settings.port,
+	  ssl      : settings.ssl
+	},
+  //searchPath: 'knex,public'
+});
+
+
+pg.select('famous_people').where('first_name', '=', argv[0]).then((err, row) => {
+	console.log(row);
+});
